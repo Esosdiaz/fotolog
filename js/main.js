b0a6b1e5ed0d537efbc685b8c64b6d7e6c769820
Fa Diaz <diazgonzalez.perla@gmail.com>
2017-05-20 21:38:58 -0400

    ultima correccion

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var nombreFF = document.getElementById("nombre").value;
 var contenidoFF = document.getElementById("cajaposteos").value;
 
 var nuevoPost = document.createElement("div");
-var contenedorNombre = document.createElement("div");
+var contenedorNombre = document.createElement("strong");
 var contenedorPost = document.createElement("p");
 var separacion = document.createElement("hr");
 
